057c2de5db6568d190a2d03a80b7f95a3058c150
Chan Teck Wei <chan.tw97@gmail.com>
2019-03-11 14:00:42 +0800

    Change AuthStateObserver.Callback from class to interface

--- app/src/main/java/com/owellox/firebit/MainActivity.kt
+++ app/src/main/java/com/owellox/firebit/MainActivity.kt
@@ -2,11 +2,20 @@ package com.owellox.firebit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseUser
+import com.owellox.firebit.auth.AuthObserver
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val authCallback = object: AuthObserver.Callback {
+            override fun onSignedIn(user: FirebaseUser) {}
+
+            override fun onSignedOut() {}
+        }
+        val authObserver = AuthObserver(lifecycle, authCallback)
+        lifecycle.addObserver(authObserver)
     }
 }
--- auth/src/main/java/com/owellox/firebit/auth/AuthObserver.java
+++ auth/src/main/java/com/owellox/firebit/auth/AuthObserver.java
@@ -2,29 +2,41 @@ package com.owellox.firebit.auth;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.OnLifecycleEvent;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class AuthObserver {
+/**
+ * Observe the state of user authentication. This is a lifecycle-aware component.
+ */
+public class AuthObserver implements LifecycleObserver {
     private final FirebaseAuth.AuthStateListener authStateListener;
     private final AuthObserver.Callback callback;
     private final Lifecycle lifecycle;
 
+    /**
+     * Connect this observer.
+     */
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     public final void connect() {
         if (this.lifecycle.getCurrentState().isAtLeast(Lifecycle.State.STARTED)) {
             FirebaseAuth.getInstance().addAuthStateListener(this.authStateListener);
         }
-
     }
 
+    /**
+     * Disconnect this observer.
+     */
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     public final void disconnect() {
         FirebaseAuth.getInstance().removeAuthStateListener(this.authStateListener);
     }
 
-    public AuthObserver(@NonNull AuthObserver.Callback callback, @NonNull Lifecycle lifecycle) {
+    /**
+     * A lifecycle-aware component for authentication state.
+     */
+    public AuthObserver(@NonNull Lifecycle lifecycle, @NonNull AuthObserver.Callback callback) {
         this.callback = callback;
         this.lifecycle = lifecycle;
         this.authStateListener = new FirebaseAuth.AuthStateListener() {
@@ -39,9 +51,23 @@ public class AuthObserver {
         };
     }
 
-    public static final class Callback {
-        public final void onSignedIn(@NonNull FirebaseUser user) {}
+    /**
+     * Pass this callback to the constructor of {@link AuthObserver} to be notified when user's authentication state
+     * has changed.
+     */
+    public interface Callback {
+        /**
+         * Called whenever right after the observer has been connected and the user is currently signed in.
+         * Additionally, this method will be called whenever the user has recently signed-in.
+         *
+         * @param user The user that's recently signed-in.
+         */
+        void onSignedIn(@NonNull FirebaseUser user);
 
-        public final void onSignedOut() {}
+        /**
+         * Called whenever right after the observer has been connected and the user is not signed in.
+         * Additionally, this method will be called whenever the user has recently signed-out.
+         */
+        void onSignedOut();
     }
 }
