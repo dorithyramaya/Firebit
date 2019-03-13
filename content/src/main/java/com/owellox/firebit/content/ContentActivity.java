caaf2a343a47aef77f69865d7c8946f6a24a174f
Chan Teck Wei <owellox@gmail.com>
2019-03-13 11:07:29 +0800

    Update documentations

--- content/src/main/java/com/owellox/firebit/content/ContentActivity.java
+++ content/src/main/java/com/owellox/firebit/content/ContentActivity.java
@@ -18,14 +18,25 @@ package com.owellox.firebit.content;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * Base class for activities that support empty state.
+ */
 public abstract class ContentActivity extends AppCompatActivity {
     private @Nullable OnEmptyStateListener onEmptyStateListener;
 
+    /**
+     * Notify this class that the content is empty.
+     */
     public final void notifyEmptyState() {
         if (onEmptyStateListener != null) {
             onEmptyStateListener.onEmptyState();
         }
     }
 
-    public final void setOnEmptyStateListener(@Nullable OnEmptyStateListener listener) {}
+    /**
+     * Set the listener to invoke when {@link #notifyEmptyState()} is called.
+     */
+    public final void setOnEmptyStateListener(@Nullable OnEmptyStateListener listener) {
+        onEmptyStateListener = listener;
+    }
 }
--- content/src/main/java/com/owellox/firebit/content/ContentFragment.java
+++ content/src/main/java/com/owellox/firebit/content/ContentFragment.java
@@ -18,15 +18,24 @@ package com.owellox.firebit.content;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+/**
+ * Base class for fragments that support empty state.
+ */
 public abstract class ContentFragment extends Fragment {
     private @Nullable OnEmptyStateListener onEmptyStateListener;
 
+    /**
+     * Notify this class that the content is empty.
+     */
     public final void notifyEmptyState() {
         if (onEmptyStateListener != null) {
             onEmptyStateListener.onEmptyState();
         }
     }
 
+    /**
+     * Set the listener to invoke when {@link #notifyEmptyState()} is called.
+     */
     public final void setOnEmptyStateListener(@Nullable OnEmptyStateListener listener) {
         onEmptyStateListener = listener;
     }
